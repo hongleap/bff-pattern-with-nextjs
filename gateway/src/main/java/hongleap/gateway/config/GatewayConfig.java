140e6c89db9aa2cb0deee3d102b2ca13733a76f1
hongleap <hongleap0000@gmail.com>
2026-01-16 00:56:16 +0700

    fix oauth2 and gateway configuration

--- gateway/src/main/java/hongleap/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/hongleap/gateway/config/GatewayConfig.java
@@ -1,6 +1,7 @@
 package hongleap.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,22 +12,15 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRoute(RouteLocatorBuilder builder){
         return builder.routes()
-                .route("order",
-                        r -> r.path("/orders/**")
-                        .filters(f ->
-                                f.rewritePath("/oder-service/?(<segment>.*)","api/v1/orders/${segment}"))
+                .route("order",r -> r.path("/api/v1/orders/**")
                         .uri("http://localhost:8081")
 
                 )
-                .route("product",
-                        r -> r.path("/products/**")
-                        .filters(f ->
-                                f.rewritePath("/product-service/?(<segment>.*)", "api/v1/products/${segment}"))
+                .route("product",r -> r.path("/api/v1/products/**")
                         .uri("http://localhost:8082")
                 )
                  //config authserver
-                .route("auth",
-                        r -> r.path("/oauth2/**")
+                .route("auth",r -> r.path("/oauth2/**")
                         .uri("http://localhost:9000")
                 )
                 .build();
