2374bed9b5b3a594786071f2abd44818a85c6c7a
hongleap <hongleap0000@gmail.com>
2026-01-15 23:19:49 +0700

    update database

--- gateway/src/main/java/hongleap/gateway/GatewayApplication.java
+++ gateway/src/main/java/hongleap/gateway/GatewayApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
--- gateway/src/main/java/hongleap/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/hongleap/gateway/config/GatewayConfig.java
@@ -11,14 +11,23 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRoute(RouteLocatorBuilder builder){
         return builder.routes()
-                .route("user", r ->
-                        r.path("/users/**")
-                                .uri("http://localhost:8081")
+                .route("order",
+                        r -> r.path("/orders/**")
+                        .filters(f ->
+                                f.rewritePath("/oder-service/?(<segment>.*)","api/v1/orders/${segment}"))
+                        .uri("http://localhost:8081")
+
                 )
-//                 config authserver
-                .route("auth", r ->
-                        r.path("/oauth2/**")
-                                .uri("http://localhost:9000")
+                .route("product",
+                        r -> r.path("/products/**")
+                        .filters(f ->
+                                f.rewritePath("/product-service/?(<segment>.*)", "api/v1/products/${segment}"))
+                        .uri("http://localhost:8082")
+                )
+                 //config authserver
+                .route("auth",
+                        r -> r.path("/oauth2/**")
+                        .uri("http://localhost:9000")
                 )
                 .build();
 
